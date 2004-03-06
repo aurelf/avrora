a0f185e695135d1bfb38da22242d8338dbcbf05a
titzer <titzer>
2004-03-06 02:40:14 +0000

    *** empty log message ***

--- src/avrora/sim/BranchCounter.java
+++ src/avrora/sim/BranchCounter.java
@@ -3,6 +3,13 @@ package avrora.sim;
 import avrora.core.Instr;
 
 /**
+ * The <code>BranchCounter</code> class is a profiling probe that can be inserted
+ * at a branch instruction to count the number of times the branch is taken and
+ * not taken. It demonstrates the ability to inspect the state of the program
+ * after the execution of a program. It determines whether the branch was taken
+ * by inspecting the program counter of the new state. If the program counter
+ * is not equal to the instruction following the branch, then the branch was
+ * taken.
  * @author Ben L. Titzer
  */
 public class BranchCounter implements Simulator.Probe {
