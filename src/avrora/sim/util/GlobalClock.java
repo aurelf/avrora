2bcca34f3d8135c8c75b878d4fb24670981076ab
titzer <titzer>
2004-04-03 22:24:07 +0000

    beginnings of global clock synchronization

--- src/avrora/sim/util/GlobalClock.java
+++ src/avrora/sim/util/GlobalClock.java
@@ -3,6 +3,9 @@ package avrora.sim.util;
 import avrora.sim.Simulator;
 import avrora.sim.SimulatorThread;
 
+import java.util.HashSet;
+import java.util.HashMap;
+
 /**
  * The <code>GlobalClock</code> class implements a global timer among multiple
  * simulators by inserting periodic timers into each simulator.
@@ -10,31 +13,55 @@ import avrora.sim.SimulatorThread;
  */
 public class GlobalClock {
 
-    int goal;
-    int count;
-    final Object condition;
+    protected double period;
+    protected int goal;
+    protected int count;
+    protected final Object condition;
+    protected final HashMap threadMap;
+    protected final DeltaQueue eventQueue;
 
-    public GlobalClock(double period, SimulatorThread[] sims) {
-
-        for ( int cntr = 0; cntr < sims.length; cntr++ ) {
-            new LocalTimer(sims[cntr].getSimulator(), period);
-        }
-
-        goal = sims.length;
+    public GlobalClock(double p) {
         condition = new Object();
+        eventQueue = new DeltaQueue();
+        period = p;
+        threadMap = new HashMap();
     }
 
     public void add(SimulatorThread t) {
-
+        // TODO: synchronization
+        if ( threadMap.containsKey(t) ) return;
+        threadMap.put(t, new LocalTimer(t.getSimulator(), period));
+        goal++;
     }
 
     public void remove(SimulatorThread t) {
-        
+        // TODO: synchronization
+        if ( !threadMap.containsKey(t) ) return;
+        LocalTimer lt = (LocalTimer)threadMap.get(t);
+        lt.remove();
+        threadMap.remove(t);
+        goal--;
+    }
+
+    public void addTimerEvent(Simulator.Trigger trigger, long ticks) {
+        // TODO: synchronization
+        eventQueue.add(trigger, ticks);
+    }
+
+    public void removeTimerEvent(Simulator.Trigger trigger) {
+        // TODO: synchronization
+        eventQueue.remove(trigger);
+    }
+
+    protected void tick() {
+        count = 0;
+        eventQueue.advance(1);
     }
 
     public class LocalTimer implements Simulator.Trigger {
         private final Simulator simulator;
         private final long cycles;
+        private boolean removed;
 
         LocalTimer(Simulator s, double period) {
             simulator = s;
@@ -53,7 +80,7 @@ public class GlobalClock {
                     }
                     else {
                         // last thread to arrive sets the count to zero and notifies all other threads
-                        count = 0;
+                        tick();
                         condition.notifyAll();
                     }
                 }
@@ -61,7 +88,13 @@ public class GlobalClock {
                 throw new InterruptedException(e);
             }
 
-            simulator.addTimerEvent(this, cycles);
+            if ( !removed )
+                simulator.addTimerEvent(this, cycles);
+        }
+
+        protected void remove() {
+            simulator.removeTimerEvent(this);
+            removed = true;
         }
 
     }
