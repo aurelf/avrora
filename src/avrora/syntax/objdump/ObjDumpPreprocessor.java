23c51efb345c1b2457fc52ff5a88463201f0a290
titzer <titzer>
2004-04-12 21:41:12 +0000

    initial skeleton of preprocessor

---
+++ src/avrora/syntax/objdump/ObjDumpPreprocessor.java
@@ -0,0 +1,11 @@
+package avrora.syntax.objdump;
+
+/**
+ * The <code>ObjDumpPreprocessor</code> class is a utility class that takens the output
+ * from the <code>avr-obdjump</code> utility and produces a cleaned up version that is
+ * more suitable for parsing into the internal format of Avrora.
+ * @author Ben L. Titzer
+ * @author Vids Samanta
+ */
+public class ObjDumpPreprocessor {
+}
