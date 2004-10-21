8df9a4a547e678aa4f4851a91a18d74754c8bcd5
titzer <titzer>
2004-10-21 03:14:29 +0000

    *** empty log message ***

--- src/avrora/Version.java
+++ src/avrora/Version.java
@@ -56,7 +56,7 @@ public class Version {
      * The <code>commit</code> field stores the commit number
      * (i.e. the number of code revisions committed to CVS since the last release).
      */
-    public final int commit = 5;
+    public final int commit = 6;
 
     public static Version getVersion() {
         return new Version();
