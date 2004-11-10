c979e86f2d948ccee9d325e246f5fc7b77aa0732
titzer <titzer>
2004-11-10 23:53:57 +0000

    adding some more benchmarking tests

---
+++ src/avrora/test/bench/nop_massive.asm
@@ -0,0 +1,69 @@
+forever:
+inner:	
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	inc r1
+	brne inner
+	inc r0
+	brne inner
+	inc r16
+	cpi r16, 250
+	brne inner
+	
+	break
+	
