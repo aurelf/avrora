0add2d2727562b30f9bdf27deaeaae74baa5f683
titzer <titzer>
2005-02-13 03:13:35 +0000

    finishing finite state machine implementation

--- src/avrora/sim/FiniteStateMachine.java
+++ src/avrora/sim/FiniteStateMachine.java
@@ -33,6 +33,8 @@
 package avrora.sim;
 
 import avrora.Avrora;
+import avrora.sim.util.MulticastFSMProbe;
+import avrora.sim.util.MulticastProbe;
 
 /**
  * The <code>FiniteStateMachine</code> class represents a model of a finite state machine that
@@ -68,36 +70,94 @@ public class FiniteStateMachine {
         public void fireAfterTransition(int beforeState, int afterState);
     }
 
+    protected class State {
+        final String name;
+        final int[] transition_time;
+        final MulticastFSMProbe probes;
+
+        State(String n, int[] tt) {
+            name = n;
+            transition_time = tt;
+            probes = new MulticastFSMProbe();
+        }
+    }
+
+    protected class TransitionEvent implements Simulator.Event {
+
+        protected int oldState;
+        protected int newState;
+
+        public void fire() {
+            // set the current state to the new state
+            curState = newState;
+            // fire any probes as necessary
+            fireAfter(states[oldState].probes, oldState, newState);
+            fireAfter(states[newState].probes, oldState, newState);
+            fireAfter(globalProbe, oldState, newState);
+        }
+
+    }
+
+    private void fireBefore(MulticastFSMProbe p, int oldState, int newState) {
+        if ( !p.isEmpty() )
+            p.fireAfterTransition(oldState, newState);
+    }
+
+    private void fireAfter(MulticastFSMProbe p, int oldState, int newState) {
+        if ( !p.isEmpty() )
+            p.fireAfterTransition(oldState, newState);
+    }
 
     public static final int IN_TRANSITION = -1;
 
-    public final int numStates;
-    public final int startState;
-    public final Clock clock;
+    protected final int numStates;
+    protected final int startState;
+    protected final Clock clock;
+    protected final TransitionEvent transEvent = new TransitionEvent();
+    protected final MulticastFSMProbe globalProbe = new MulticastFSMProbe();
+    protected State[] states;
+
     protected int curState;
 
-    public FiniteStateMachine(Clock c, int ss, int ns) {
+    public FiniteStateMachine(Clock c, int ss, String[] nm, int tt) {
         clock = c;
         startState = ss;
         curState = ss;
-        numStates = ns;
+        numStates = nm.length;
+        states = new State[numStates];
+        int[][] ttm = buildUniformTTM(numStates, tt);
+        buildStates(nm, ttm);
     }
 
+    public FiniteStateMachine(Clock c, int ss, String[] nm, int[][] ttm) {
+        clock = c;
+        startState = ss;
+        curState = ss;
+        numStates = nm.length;
+        states = new State[numStates];
+        buildStates(nm, ttm);
+    }
+
+    private void buildStates(String[] nm, int[][] ttm) {
+        for ( int cntr = 0; cntr < numStates; cntr++ ) {
+            states[cntr] = new State(nm[cntr], ttm[cntr]);
+        }
+    }
 
     public void insertProbe(Probe p) {
-        throw Avrora.unimplemented();
+        globalProbe.add(p);
     }
 
     public void removeProbe(Probe p) {
-        throw Avrora.unimplemented();
+        globalProbe.remove(p);
     }
 
     public void insertProbe(Probe p, int state) {
-        throw Avrora.unimplemented();
+        states[state].probes.add(p);
     }
 
     public void removeProbe(Probe p, int state) {
-        throw Avrora.unimplemented();
+        states[state].probes.remove(p);
     }
 
     public int getNumberOfStates() {
@@ -112,19 +172,143 @@ public class FiniteStateMachine {
         return curState;
     }
 
-    public void transition(int newstate) {
-        throw Avrora.unimplemented();
+    public void transition(int newState) {
+        // are we currently in a transition already?
+        if ( curState == IN_TRANSITION ) {
+            throw Avrora.failure("cannot transition to state "
+                    +newState+" while in transition: "
+                    +transEvent.oldState+" -> "+transEvent.newState);
+        }
+
+        // get transition time
+        int ttime = states[curState].transition_time[newState];
+        if ( ttime < 0 ) // valid transition ?
+            throw Avrora.failure("cannot transition from state "
+                    +curState+" -> "+newState);
+
+        // fire probes before transition
+        fireBefore(globalProbe, curState, newState);
+        fireBefore(states[curState].probes, curState, newState);
+        fireBefore(states[newState].probes, curState, newState);
+
+        if ( ttime == 0 ) {
+            // transition is instantaneous
+            curState = newState;
+        }
+        else {
+            // transition will complete in the future
+            transEvent.oldState = curState;
+            transEvent.newState = newState;
+            clock.insertEvent(transEvent, ttime);
+        }
     }
 
     public int getTransitionTime(int beforeState, int afterState) {
-        throw Avrora.unimplemented();
+        return states[beforeState].transition_time[afterState];
     }
 
     public String getStateName(int state) {
-        throw Avrora.unimplemented();
+        return states[state].name;
     }
 
     public Clock getClock() {
         return clock;
+    }
+
+    /**
+     * The <code>buildUniformTTM()</code> method builds a transition time
+     * matrix that is uniform; the machine can transition from any state to any other
+     * state with the given transition time.
+     * @param size the size of the transition time matrix
+     * @param tt the transition time for each edge
+     * @return a new transition time matrix where each entry is the given transition time
+     */
+    public static int[][] buildUniformTTM(int size, int tt) {
+        int[][] ttm = new int[size][size];
+        for ( int cntr = 0; cntr < size; cntr++ ) {
+            for ( int loop = 0; loop < size; loop++ ) {
+                ttm[cntr][loop] = tt;
+            }
+        }
+        return ttm;
+    }
+
+    /**
+     * The <code>buildBimodalTTM()</code> method builds a transition time matrix
+     * that corresponds to a finite state machine with two modes. One special state
+     * is the "default" state. The machine can transition from the default state to
+     * any other state, and from any other state back to the default state, but not
+     * between any other two states.
+     * @param size the size of the transition time matrix
+     * @param ds the default state
+     * @param tf the transition times from each state back to the default state
+     * @param tt the transition times from the default state to each other state
+     * @return a square transition time matrix that represents a bimodal state machine
+     */
+    public static int[][] buildBimodalTTM(int size, int ds, int[] tf, int[] tt) {
+        int[][] ttm = newTTM(size);
+
+        for ( int cntr = 0; cntr < size; cntr++ ) {
+            for ( int loop = 0; loop < size; loop++ ) {
+                ttm[cntr][ds] = tf[cntr];
+                ttm[ds][cntr] = tt[cntr];
+            }
+        }
+        return ttm;
+    }
+
+    /**
+     * The <code>setCircularTTM()</code> method builds a transition time matrix
+     * that represents a finite state machine arranged in a ring; each state can transition
+     * to one other state, wrapping around. For example, a finite state machine of consisting
+     * of states S1, S2, and S3 could have a cycle S1 -> S2 -> S3 -> S1.
+     * @param ttm the original transition time matrix
+     * @param perm an array of integers representing the order of the state transitions in
+     * the ring
+     * @param tt the transition time between the corresponding states in the ring
+     * @return a square
+     */
+    public static int[][] setCircularTTM(int[][] ttm, int[] perm, int[] tt) {
+        int size = ttm.length;
+
+        for ( int cntr = 0; cntr < size-1; cntr++ ) {
+            ttm[perm[cntr]][perm[cntr+1]] = tt[perm[cntr+1]];
+        }
+        ttm[perm[size-1]][perm[0]] = tt[perm[0]];
+        return ttm;
+    }
+
+    /**
+     * The <code>newTTM()</code> method is a utility function for building a new
+     * transition time matrix. It will create a new transition time matrix (TTM) where
+     * each entry is <code>-1</code>, indicating that there are no legal state transitions.
+     * @param size the size of matrix, i.e. the number of rows, which is equal to the
+     * number of columns
+     * @return a square matrix of the given size where each entry is set to -1
+     */
+    public static int[][] newTTM(int size) {
+        int[][] ttm = new int[size][size];
+
+        for ( int cntr = 0; cntr < size; cntr++ ) {
+            for ( int loop = 0; loop < size; loop++ ) {
+                ttm[cntr][loop] = -1;
+            }
+        }
+        return ttm;
+    }
+
+    /**
+     * The <code>setDiagonal()</code> method sets the diagonal of the given transition
+     * time matrix to the specified value. This is useful for finite state machines where
+     * transitions from one state to the same state is either impossible or a no-op.
+     * @param ttm the original transition time matrix
+     * @param diag the value to set the diagonal entries to
+     * @return the original transition time matrix with the diagonal entries appropriately
+     * set
+     */
+    public static int[][] setDiagonal(int[][] ttm, int diag) {
+        for ( int cntr = 0; cntr < ttm.length; cntr++ )
+            ttm[cntr][cntr] = diag;
+        return ttm;
     }
 }
---
+++ src/avrora/sim/util/MulticastFSMProbe.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2004-2005, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the University of California, Los Angeles nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package avrora.sim.util;
+
+import avrora.sim.FiniteStateMachine;
+import avrora.core.Instr;
+import avrora.sim.Simulator;
+import avrora.sim.State;
+import avrora.sim.FiniteStateMachine;
+
+/**
+ * The <code>MulticastProbe</code> is a wrapper around multiple probes that allows them to act as a single
+ * probe. It is useful for composing multiple probes into one and is used internally in the simulator.
+ *
+ * @author Ben L. Titzer
+ * @see avrora.sim.FiniteStateMachine
+ */
+public class MulticastFSMProbe implements FiniteStateMachine.Probe {
+
+    /**
+     * The <code>Link</code> class is used internally to implement the linked list of the probes. It exists
+     * because a simple, custom list structure allows for the most efficient dispatching code possible.
+     * Performance is critical since the multicast probe may be for every instruction executed in the
+     * simulator.
+     */
+    private static class Link {
+        final FiniteStateMachine.Probe probe;
+        Link next;
+
+        Link(FiniteStateMachine.Probe p) {
+            probe = p;
+        }
+    }
+
+    private Link head;
+    private Link tail;
+
+    /**
+     * The <code>add()</code> method allows another probe to be inserted into the multicast set. It will be
+     * inserted at the end of the list of current probes and will therefore fire after any probes already in
+     * the multicast set.
+     *
+     * @param b the probe to insert
+     */
+    public void add(FiniteStateMachine.Probe b) {
+        if (b == null) return;
+
+        if (head == null) {
+            head = tail = new Link(b);
+        } else {
+            tail.next = new Link(b);
+            tail = tail.next;
+        }
+    }
+
+    /**
+     * The <code>remove</code> method removes a probe from the multicast set. The order of the remaining
+     * probes is not changed. The comparison used is reference equality, not the <code>.equals()</code>
+     * method.
+     *
+     * @param b the probe to remove
+     */
+    public void remove(FiniteStateMachine.Probe b) {
+        if (b == null) return;
+
+        Link prev = null;
+        Link pos = head;
+        while (pos != null) {
+            Link next = pos.next;
+
+            // matched?
+            if (pos.probe == b) {
+                // remove the head ?
+                if (prev == null) head = pos.next;
+                // somewhere in the middle (or at end)
+                else prev.next = pos.next;
+                // remove the tail item ?
+                if (pos == tail) tail = prev;
+
+            } else {
+                // no match; continue
+                prev = pos;
+            }
+            pos = next;
+        }
+    }
+
+    /**
+     * The <code>isEmpty()</code> method tests whether the multicast set of this probe is empty.
+     *
+     * @return false otherwise
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void fireBeforeTransition(int beforeState, int afterState) {
+        for (Link pos = head; pos != null; pos = pos.next)
+            pos.probe.fireBeforeTransition(beforeState, afterState);
+    }
+
+    public void fireAfterTransition(int beforeState, int afterState) {
+        for (Link pos = head; pos != null; pos = pos.next)
+            pos.probe.fireAfterTransition(beforeState, afterState);
+    }
+}
