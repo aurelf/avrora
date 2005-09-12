83fb0b6637a41c6af52a2b4b2fe23f2877529ce4
titzer <titzer>
2005-09-12 23:16:37 +0000

    Initial revision

---
+++ src/cck/help/HelpCategory.java
@@ -0,0 +1,253 @@
+/**
+ * Copyright (c) 2004-2005, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the University of California, Los Angeles nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cck.help;
+
+import cck.text.StringUtil;
+import cck.text.Terminal;
+import cck.util.*;
+import java.util.*;
+
+/**
+ * The <code>HelpCategory</code> class implements a category that provides help from the
+ * command line. It can contain multiple sections, including sections on options, and a list
+ * of other help items (such as what the values of each option does).
+ *
+ * @author Ben L. Titzer
+ */
+public class HelpCategory implements HelpItem {
+
+    public String name;
+    public final String help;
+
+    private final LinkedList sections;
+
+    public static final Comparator COMPARATOR = new Comparator() {
+        public int compare(Object o1, Object o2) {
+            HelpCategory c1 = (HelpCategory)o1;
+            HelpCategory c2 = (HelpCategory)o2;
+            return String.CASE_INSENSITIVE_ORDER.compare(c1.name, c2.name);
+        }
+    };
+
+    private abstract class Section {
+        abstract void printHelp();
+    }
+
+    private class ParagraphSection extends Section {
+        final String title;
+        final String para;
+
+        ParagraphSection(String t, String p) {
+            title = t;
+            para = p;
+        }
+
+        void printHelp() {
+            if ( title != null ) {
+                Terminal.printBrightBlue(title);
+                Terminal.println("\n");
+            }
+
+            Terminal.println(StringUtil.makeParagraphs(para, 0, 4, Terminal.MAXLINE));
+            Terminal.println("");
+        }
+    }
+
+    private class OptionsSection extends Section {
+        final String para;
+        final Options options;
+
+        OptionsSection(String para, Options opts) {
+            this.para = para;
+            this.options = opts;
+        }
+
+        void printHelp() {
+            Terminal.printBrightBlue("OPTIONS");
+            Terminal.println("\n");
+
+            Terminal.println(StringUtil.makeParagraphs(para, 0, 4, Terminal.MAXLINE));
+            Terminal.println("");
+
+            Collection c = options.getAllOptions();
+            List l = Collections.list(Collections.enumeration(c));
+            Collections.sort(l, Option.COMPARATOR);
+
+            Iterator i = l.iterator();
+            while (i.hasNext()) {
+                Option opt = (Option)i.next();
+                opt.printHelp();
+            }
+
+            Terminal.println("");
+        }
+    }
+
+    private class ListSection extends Section {
+        final String title;
+        final String para;
+        final List list;
+
+        ListSection(String t, String p, List l) {
+            title = t;
+            para = p;
+            list = l;
+        }
+
+        void printHelp() {
+            if ( title != null ) {
+                Terminal.printBrightBlue(title);
+                Terminal.println("\n");
+            }
+
+            Terminal.println(StringUtil.makeParagraphs(para, 0, 4, Terminal.MAXLINE));
+            Terminal.println("");
+
+            Iterator i = list.iterator();
+            while (i.hasNext()) {
+                HelpItem hi = (HelpItem)i.next();
+                hi.printHelp();
+            }
+
+            Terminal.println("");
+        }
+    }
+
+    /**
+     * The constructor for the <code>HelpCategory</code> class creates a new help category with the specified
+     * short name and the specified default help.
+     * @param name the short name of this category (where it is accessible from the command line)
+     * @param help the help description for this category
+     */
+    public HelpCategory(String name, String help) {
+        this.name = name;
+        this.help = help;
+        this.sections = new LinkedList();
+    }
+
+    /**
+     * The <code>getHelp()</code> method returns a string representing help for this help item.
+     * @return a help string for this item
+     */
+    public String getHelp() {
+        return help;
+    }
+
+    /**
+     * The <code>getName()</code> method returns the short name for this help category. This short name
+     * is used to add this help category to the global help category database.
+     * @return the short name of this help category
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * The <code>setName()</code> method is used to set the short name for this help category.
+     * @param nm the new short name for this category
+     */
+    public void setName(String nm) {
+        name = nm;
+    }
+
+    /**
+     * The <code>addSection()</code> method adds a new section to this help category with the specified title
+     * and a paragraph that is automatically formatted when printed out.
+     * @param title the title of the new section
+     * @param paragraph a long string representing the text for this section
+     */
+    public void addSection(String title, String paragraph) {
+        sections.addLast(new ParagraphSection(title, paragraph));
+    }
+
+    /**
+     * The <code>addOptionSection()</code> method adds a new section to this help category with the specified
+     * options. The new section will contain a paragraph description of the options and list the options in
+     * alphabetical order.
+     * @param para a summary of the options
+     * @param opts the options for this help category
+     */
+    public void addOptionSection(String para, Options opts) {
+        sections.addLast(new OptionsSection(para, opts));
+    }
+
+    /**
+     * The <code>addListSection()</code> method adds a new section to this help category with the specified
+     * list of help items.
+     * @param title the title of the new sectiobn
+     * @param para a paragraph description of the section
+     * @param l a list of <code>HelpItem</code> instances that will be added to the end of the section
+     */
+    public void addListSection(String title, String para, List l) {
+        sections.addLast(new ListSection(title, para, l));
+    }
+
+    /**
+     * The <code>addSubcategorySection</code> method adds a new section that is a list of subcategories
+     * under this main category.
+     * @param title the title of the section
+     * @param para a paragraph description of this section
+     * @param l a list of subcategories
+     */
+    public void addSubcategorySection(String title, String para, List l) {
+        Iterator i = l.iterator();
+        LinkedList sl = new LinkedList();
+        while ( i.hasNext() ) {
+            HelpCategory hc = (HelpCategory)i.next();
+            sl.addLast(new SubcategoryItem(4, hc));
+        }
+        addListSection(title, para, sl);
+    }
+
+    /**
+     * The <code>addCommandExample()</code> method adds a command example, properly formatted, to
+     * this section.
+     * @param prefix  a string that is the prefix to the command line
+     * @param command the command line example to add
+     */
+    public void addCommandExample(String prefix, String command) {
+        throw Util.unimplemented();
+    }
+
+    /**
+     * The <code>printHelp()</code> method prints out all of the help sections in order for this category.
+     */
+    public void printHelp() {
+        Iterator i = sections.iterator();
+        while ( i.hasNext() ) {
+            Section s = (Section)i.next();
+            s.printHelp();
+        }
+    }
+
+}
---
+++ src/cck/stat/Proportion.java
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) 2004-2005, Regents of the University of California
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the University of California, Los Angeles nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cck.stat;
+
+import cck.text.Terminal;
+import java.util.Vector;
+
+/**
+ * This class represents the proportion of different items with respect to one another. For example, the
+ * proportion of cars, trucks, vans, etc on the highway.
+ *
+ * @author Ben L. Titzer
+ */
+public class Proportion extends DataItem {
+
+    protected Vector shares;
+    protected int total;
+
+    /**
+     * Internal class that encapsulates both a counter and the fraction of the total that this named Share
+     * represents.
+     */
+    public class Share extends Counter {
+        float fraction;
+
+        public Share(String newname) {
+            super(newname);
+        }
+
+        public Share(String newname, int count) {
+            super(newname, count);
+        }
+
+        public float getFraction() {
+            return fraction;
+        }
+
+        public void processData() {
+            Proportion.this.processData();
+        }
+
+        public void textReport() {
+            Terminal.print("\n   " + name + ": " + count + ", " + (100 * fraction) + '%');
+        }
+    }
+
+    /**
+     * Public constructor that takes a string name.
+     */
+    public Proportion(String newname) {
+        name = newname;
+        shares = new Vector(5);
+    }
+
+    /**
+     * Generate a text report of the shares.
+     */
+    public void textReport() {
+        int numshares = shares.size();
+        Terminal.print("\n " + name);
+        Terminal.print("\n---------------------------------------------------------------------");
+
+        for (int cntr = 0; cntr < numshares; cntr++) {
+            Share s = (Share)shares.get(cntr);
+            s.textReport();
+        }
+
+        Terminal.print("\n");
+    }
+
+    /**
+     * Register a counter object with this proportion.
+     */
+    public Share createShare(String name) {
+        Share s = new Share(name);
+        shares.add(s);
+        return s;
+    }
+
+    /**
+     * Register an integer count with this proportion object and return a Counter object.
+     */
+    public Share registerCount(String str, int count) {
+        Share s = new Share(str, count);
+        shares.add(s);
+        return s;
+    }
+
+    /**
+     * Search for the counter with the specified string name and return it if it is registered.
+     */
+    public Share getShareForName(String name) {
+        int numshares = shares.size();
+
+        for (int i = 0; i < numshares; i++) {
+            Share s = (Share)shares.get(i);
+            if (name.equals(s.getName())) return s;
+        }
+
+        return null;
+    }
+
+    /**
+     * Search for the counter with the specified name and report its proportion. Return -1 if not found.
+     */
+    public float getFractionForName(String name) {
+        int numshares = shares.size();
+
+        processData(); // make sure proportions up to date
+
+        for (int i = 0; i < numshares; i++) {
+            Share s = (Share)shares.get(i);
+            if (name.equals(s.getName())) return s.fraction;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Do the computations and compute the proportions of each.
+     */
+    public void processData() {
+        int numshares = shares.size();
+        int tmptotal = 0;
+
+        for (int i = 0; i < numshares; i++) {
+            Share s = (Share)shares.get(i);
+            tmptotal += s.getTotal();
+        }
+
+        total = tmptotal;
+
+        for (int i = 0; i < numshares; i++) {
+            Share s = (Share)shares.get(i);
+            float f = ((float)s.getTotal()) / ((float)total);
+            s.fraction = f;
+        }
+
+    }
+
+    /**
+     * Return true if this proportion has any information available.
+     */
+    public boolean hasData() {
+        return (shares.size() > 0);
+    }
+}
