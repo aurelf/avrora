63dc1dcc5a71a427635fa4442e1f7f28d41e51db
titzer <titzer>
2006-05-30 14:11:18 +0000

    version bump due to cck updates

--- src/avrora/Version.java
+++ src/avrora/Version.java
@@ -52,7 +52,7 @@ public class Version {
      * The <code>commit</code> field stores the commit number (i.e. the number of code revisions committed to
      * CVS since the last release).
      */
-    public static final int commit = 57;
+    public static final int commit = 59;
 
     /**
      * The <code>TAG</code> field stores a reference to the version tag for the current
